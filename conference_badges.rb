99946751943bd234b331126ff25c706bcaf705e0
Kyle Harris <k_harris95@live.com>
2018-01-17 20:46:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+  badge_arr = []
+  name_arr.each do |name|
+    badge_arr << badge_maker(name)
+  end
+end
